37eb83bbb9c8bf73ac3feddb3d4e9f85b8719225
Mubasher55 <mubkhan1214@gmail.com>
2026-05-27 11:29:57 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'move_home_screen.dart';
 
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MoveApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MoveHomeScreen(),
+      body: Center(
+        Text("Welcome to MoveApp 🚀")),
+          child: Image.asset("assets/bg.jpg"),
+    );
+  }
+}

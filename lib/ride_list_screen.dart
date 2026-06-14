f421ce83772de4f4d1060ddb66eda990a324b377
Mubasher55 <mubkhan1214@gmail.com>
2026-06-14 18:35:41 +0800

    Update

--- lib/ride_list_screen.dart
+++ lib/ride_list_screen.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class RideListScreen extends StatelessWidget {
+  const RideListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("All Rides")),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("rides").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          var docs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              var data = docs[index];
+
+              return ListTile(
+                title: Text(data["pickup"]),
+                subtitle: Text(data["drop"]),
+                trailing: Text(data["fare"]),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}

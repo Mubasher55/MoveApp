f32e3c2ac5126b2173a6f6da75dda84445a9a254
Mubasher55 <mubkhan1214@gmail.com>
2026-06-15 13:16:58 +0800

    Update

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,45 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? mapController;
+
+  static const LatLng initialPosition =
+      LatLng(31.5204, 74.3587); // Lahore default
+
+  LatLng currentPosition = initialPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLocation();
+  }
+
+  Future<void> getUserLocation() async {
+    LocationPermission permission =
+        await Geolocator.requestPermission();
+
+    Position position =
+        await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      currentPosition =
+          LatLng(position.latitude, position.longitude);
+    });
+
+    mapController?.animateCamera(
+      CameraUpdate.newLatLng(currentPosition),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Move Map"),
+        title: const Text("Select Location"),
         backgroundColor: Colors.orange,
       ),
 
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(31.5204, 74.3587),
-          initialZoom: 13,
+      body: GoogleMap(
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(31.5204, 74.3587),
+          zoom: 14,
         ),
 
-        children: [
-          TileLayer(
-            urlTemplate:
-                "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-          ),
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
+      ),
 
-          MarkerLayer(
-            markers: [
-              Marker(
-                point: LatLng(31.5204, 74.3587),
-                width: 50,
-                height: 50,
-                child: const Icon(
-                  Icons.location_pin,
-                  color: Colors.red,
-                  size: 40,
-                ),
-              ),
-            ],
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        child: const Icon(Icons.my_location),
+
+        onPressed: () async {
+          await getUserLocation();
+        },
       ),
     );
   }

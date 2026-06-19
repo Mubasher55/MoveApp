6990573b2b65c3f31780bbd68c3e22ff91a92bd5
Mubasher55 <mubkhan1214@gmail.com>
2026-06-19 21:54:32 +0800

    Update

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,88 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
 
   Future login() async {
-
     try {
-
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email.text.trim(),
         password: password.text.trim(),
       );
 
-      print("Login Success");
+      // ❌ Navigator NOT needed (StreamBuilder will auto move screen)
 
     } catch (e) {
-
-      print(e);
-
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.toString())),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       backgroundColor: Colors.black,
-
       body: Padding(
-        padding: EdgeInsets.all(20),
-
+        padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-
-            Text(
+            const Text(
               "MOVE APP LOGIN",
-              style: TextStyle(
-                color: Colors.orange,
-                fontSize: 28,
-              ),
+              style: TextStyle(color: Colors.orange, fontSize: 28),
             ),
-
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
 
             TextField(
               controller: email,
-              style: TextStyle(color: Colors.white),
-
-              decoration: InputDecoration(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
                 hintText: "Email",
                 hintStyle: TextStyle(color: Colors.grey),
               ),
             ),
 
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
 
             TextField(
               controller: password,
               obscureText: true,
-              style: TextStyle(color: Colors.white),
-
-              decoration: InputDecoration(
+              style: const TextStyle(color: Colors.white),
+              decoration: const InputDecoration(
                 hintText: "Password",
                 hintStyle: TextStyle(color: Colors.grey),
               ),
             ),
 
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
 
             ElevatedButton(
               onPressed: login,
-              child: Text("LOGIN"),
+              child: const Text("LOGIN"),
             ),
-
           ],
         ),
       ),

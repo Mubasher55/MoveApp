b858ea99dcca328ab7e90ee8586f6dee9aad5949
Mubasher55 <mubkhan1214@gmail.com>
2026-06-19 21:56:46 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'firebase_options.dart';
 import 'move_home_screen.dart';
 import 'login_screen.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +28,34 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         useMaterial3: true,
       ),
-      home: const AuthWrapper(),
+      home: const AuthWrapper(), // 🔥 MAIN FIX HERE
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // loading state
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // user logged in
+        if (snapshot.hasData) {
+          return const MoveHomeScreen();
+        }
+
+        // not logged in
+        return const LoginScreen();
+      },
     );
   }
 }
